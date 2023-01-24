2ea6832df0d051466b29ed76b1fdcc7cb8f9fe3a
khalilgreenidge <khalilgreenidge16@gmail.com>
2023-01-24 06:30:09 +0000

    init changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Video } from './components/Video';
+import { Menu } from './components/Menu';
+
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
 
 function App() {
+  const [src, setSrc] = useState(VIDEOS.fast);
+
+  const chooseVideo = (newVideo) => {
+    setSrc(VIDEOS[newVideo]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <h1>Video Player</h1>
+        <Menu chooseVideo={chooseVideo}/>
+        <Video src={src}/>
+      </div>
   );
 }
 
